7fb810757229087df6d3c4ba42c7da267c716b70
faruqhossen30 <faruqhossen30@gmail.com>
2024-03-06 15:27:25 +0600

    post bug fixed

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -45,6 +45,9 @@
                                             class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase">Title
                                         </th>
                                         <th scope="col"
+                                            class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase">Status
+                                        </th>
+                                        <th scope="col"
                                             class="px-2 py-3 text-right text-xs font-medium text-gray-500 uppercase">Action
                                         </th>
                                     </tr>
@@ -56,12 +59,48 @@
                                             <td
                                                 class="px-3 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{{$posts->firstItem() + $loop->index}}</td>
                                                 <td
-                                                class="px-3 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                                                <img src="{{asset('uploads/post/'.$post->thumbnail)}}" class="h-6 w-auto" alt="">
+                                                class="px-3 py-2  text-sm font-medium text-gray-800 dark:text-gray-200">
+                                                <img src="{{asset('uploads/post/'.$post->thumbnail)}}" class="h-6 w-auto" alt="photo">
                                             </td>
                                             <td
                                                 class="px-3 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                                                {{$post->title}}</td>
+
+
+                                               {{ Str::limit( $post->title , 40)}}</td>
+
+
+
+                                                <td
+                                                class="px-3 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                                                @if ($post->status)
+                                                    <span
+                                                        class="py-1 px-2 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
+                                                        <svg class="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg"
+                                                            width="16" height="16" viewBox="0 0 24 24" fill="none"
+                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path
+                                                                d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
+                                                            <path d="m9 12 2 2 4-4" />
+                                                        </svg>
+                                                        Active
+                                                    </span>
+                                                @else
+                                                    <span
+                                                        class="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-red-100 text-red-800 rounded-full dark:bg-red-500/10 dark:text-red-500">
+                                                        <svg class="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg"
+                                                            width="16" height="16" viewBox="0 0 24 24" fill="none"
+                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round">
+                                                            <path
+                                                                d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
+                                                            <path d="M12 9v4" />
+                                                            <path d="M12 17h.01" />
+                                                        </svg>
+                                                        Deactive
+                                                    </span>
+                                                @endif
+                                            </td>
 
                                             <td class="px-3 py-2 whitespace-nowrap text-right text-sm font-medium">
                                                 <x-table.action_button route="post" :id="$post->id"/>
